ea43c1ae66b734a2bab889bd558e998b7770859b
GDias Linux <diasgeovane7@gmail.com>
2025-07-15 19:55:48 -0300

    Alterado o nome do método de DADOS_API par obterDados

--- src/main/java/br/com/alura/tabelafipe/service/ConsumoAPI.java
+++ src/main/java/br/com/alura/tabelafipe/service/ConsumoAPI.java
@@ -7,7 +7,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsumoAPI {
-    public static String DADOS_API (String endereco) {
+    public static String obterDados (String endereco) {
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
