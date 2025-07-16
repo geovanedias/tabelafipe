ddf1c98d38fce87153ff4678542126a5ae18d7c2
GDias Linux <diasgeovane7@gmail.com>
2025-07-15 22:07:17 -0300

    Alternado a exibição para uma melhor visualização dos dados

--- src/main/java/br/com/alura/tabelafipe/model/Dados.java
+++ src/main/java/br/com/alura/tabelafipe/model/Dados.java
@@ -3,6 +3,6 @@ package br.com.alura.tabelafipe.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 
 public record Dados(
-    @JsonAlias("code") String codigo,
-    @JsonAlias("name") String nome) {
+    @JsonAlias("name") String nome,
+    @JsonAlias("code") String codigo) {
 }
