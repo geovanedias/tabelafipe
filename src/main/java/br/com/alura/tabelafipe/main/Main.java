f4fdda2ccbbcb0630a549073909b5abea99b1396
GDias Linux <diasgeovane7@gmail.com>
2025-07-17 23:35:32 -0300

    Finalizado a primeira versão da aplicação

--- src/main/java/br/com/alura/tabelafipe/main/Main.java
+++ src/main/java/br/com/alura/tabelafipe/main/Main.java
@@ -1,11 +1,14 @@
 package br.com.alura.tabelafipe.main;
 
 import br.com.alura.tabelafipe.model.Dados;
+import br.com.alura.tabelafipe.model.Veiculo;
 import br.com.alura.tabelafipe.service.ConsumoAPI;
 import br.com.alura.tabelafipe.service.ConverteDados;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
     private final Scanner scanner = new Scanner(System.in);
@@ -30,18 +33,18 @@ public class Main {
 
         String endereco;
 
-        if (opcao.toLowerCase().contains("carr")) {     // TODO: trocar para usar número ao invés de digitar
+        if (opcao.toLowerCase().contains("car")) {
             endereco = URL_BASE + "cars/brands/";
         } else if (opcao.toLowerCase().contains("mot")) {
             endereco = URL_BASE + "motorcycles/brands/";
         } else {
-            endereco = URL_BASE + "truks/brands/";
+            endereco = URL_BASE + "trucks/brands/";
         }
 
-        var json = consumo.obterDados(endereco);
-        System.out.println(json);
+        AtomicReference<String> json = new AtomicReference<>(consumo.obterDados(endereco));
+        System.out.println(json.get());
 
-        var marcas = conversor.obterLista(json, Dados.class);
+        var marcas = conversor.obterLista(json.get(), Dados.class);
 
         marcas.stream()
                 .sorted(Comparator.comparing(Dados::nome))
@@ -49,6 +52,44 @@ public class Main {
 
         System.out.println("Informe o código da marca para consulta: ");
         var codigoMarca = scanner.nextInt();
+        scanner.nextLine();
+
+        endereco = endereco + codigoMarca + "/models";
+        json.set(consumo.obterDados(endereco));
+
+        var modeloLista = conversor.obterLista(json.get(), Dados.class);
+
+        System.out.println("\nModelos dessa marca:");
+        modeloLista.forEach(System.out::println);
+
+        System.out.println("\nDigite o nome do modelo que gostaria de filtrar");
+        var nomeVeiculo = scanner.nextLine();
+
+        List<Dados> modelosFiltrados = modeloLista.stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                    .toList();
+
+        System.out.println("\nModelos encontrados");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("\nDigite o código do modelo que gostaria encontrar:");
+        var codigoModelo = scanner.nextInt();
+        scanner.nextLine();
+
+        endereco = endereco + "/" + codigoModelo + "/years/";
+
+        json.set(consumo.obterDados(endereco));
+        List<Dados> anos = conversor.obterLista(json.get(), Dados.class);
+
+        String finalEndereco = endereco;
+
+        anos.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(c -> {
+                    String respostaJson = consumo.obterDados(finalEndereco + c.codigo());
+                    Veiculo veiculo = conversor.obterDados(respostaJson, Veiculo.class);
+                    System.out.println(veiculo);
+                });
 
         scanner.close();
     }
